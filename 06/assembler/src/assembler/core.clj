80fcd5838df09d0d7fe1614a025db0c8d7fc1178
johnervine@hotmail.com <johnervine@hotmail.com>
2016-10-30 11:59:58 +0000

    function added to translate A instruction into binary representation

--- 06/assembler/src/assembler/core.clj
+++ 06/assembler/src/assembler/core.clj
@@ -1,6 +1,19 @@
 (ns assembler.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.pprint :as p])
   (:gen-class))
+
+(defn- translate-A-instruction
+  "Translates A instruction into 16 bit binary representation"
+  [inst]
+  (->> (map str (rest inst))
+       (reduce str)
+       read-string
+       (p/cl-format nil "~2,15,'0r")
+       (str "0")))
+
+(defn- translate-C-instruction [inst]
+  "111")
 
 (defn- remove-whitespace [file]
   (->> (str/split-lines (slurp file))
@@ -11,11 +24,17 @@
       (subs 0 2)
       (not= "//")))
 
+(defn- translate-to-binary [inst]
+  (case (first inst)
+    \@ (translate-A-instruction inst)
+    (translate-C-instruction inst)))
+
 (defn -main
   "Entry Point"
   [& args]
   (let [insts (->> (remove-whitespace (first args))
-                   (filterv not-comment?))]
+                   (filterv not-comment?)
+                   (mapv translate-to-binary))]
     insts))
 
 (-main "/home/johnboy14/coursera/nand2tetris/06/add/Add.asm")
