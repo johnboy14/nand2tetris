ce4f361b69787142d23420f8e96c0e34d304a710
johnervine@hotmail.com <johnervine@hotmail.com>
2016-10-30 18:33:06 +0000

    Completed Assembler program for symbol free programs.

--- 06/assembler/src/assembler/core.clj
+++ 06/assembler/src/assembler/core.clj
@@ -3,6 +3,84 @@
             [clojure.pprint :as p])
   (:gen-class))
 
+(def comp-table
+  {"0"   "0101010"
+   "1"   "0111111"
+   "-1"  "0111010"
+   "D"   "0001100"
+   "A"   "0110000"
+   "!D"  "0001101"
+   "!A"  "0110001"
+   "-D"  "0001111"
+   "-A"  "0110011"
+   "D+1" "0011111"
+   "A+1" "0110111"
+   "D-1" "0001110"
+   "A-1" "0110010"
+   "D+A" "0000010"
+   "D-A" "0010011"
+   "A-D" "0000111"
+   "D&A" "0000000"
+   "D|A" "0010101"
+   "M"   "1110000"
+   "!M"  "1110001"
+   "-M"  "1110011"
+   "M+1" "1110111"
+   "M-1" "1110010"
+   "D+M" "1000010"
+   "D-M" "1010011"
+   "M-D" "1000111"
+   "D&M" "1000000"
+   "D|M" "1010101"})
+
+(def dest-table
+  {nil   "000"
+   "M"   "001"
+   "D"   "010"
+   "MD"  "011"
+   "A"   "100"
+   "AM"  "101"
+   "AD"  "110"
+   "AMD" "111"})
+
+(def jmp-table
+  {nil   "000"
+   "JGT" "001"
+   "JEQ" "010"
+   "JGE" "011"
+   "JLT" "100"
+   "JNE" "101"
+   "JLE" "110"
+   "JMP" "111"})
+
+(defn- dest-inst [inst]
+  (let [equals-idx (str/index-of inst "=")]
+    (if equals-idx
+      (subs inst 0 equals-idx))))
+
+(defn- comp-inst [inst]
+  (let [equals-idx (str/index-of inst "=")
+        equals-idx (if equals-idx (+ 1 equals-idx) 0)
+        jmp-prefix (str/index-of inst ";")]
+    (if jmp-prefix
+      (subs inst equals-idx jmp-prefix)
+      (subs inst equals-idx (count inst)))))
+
+(defn- jmp-inst [inst]
+  (let [jmp-prefix (str/index-of inst ";")]
+    (if jmp-prefix
+      (subs inst (+ 1 jmp-prefix) (count inst)))))
+
+(defn- translate-comp-inst [inst])
+
+(defn- translate-C-instruction
+  "Translates C instruction into 16 bit binary representation"
+  [inst]
+  (-> (str "111")
+      (str (comp-table (comp-inst inst)))
+      (str (dest-table (dest-inst inst)))
+      (str (jmp-table (jmp-inst inst)))))
+
 (defn- translate-A-instruction
   "Translates A instruction into 16 bit binary representation"
   [inst]
@@ -11,9 +89,6 @@
        read-string
        (p/cl-format nil "~2,15,'0r")
        (str "0")))
-
-(defn- translate-C-instruction [inst]
-  "111")
 
 (defn- remove-whitespace [file]
   (->> (str/split-lines (slurp file))
@@ -29,12 +104,31 @@
     \@ (translate-A-instruction inst)
     (translate-C-instruction inst)))
 
+(defn- write-to-file [seq file]
+  (with-open [wr (clojure.java.io/writer file)]
+    (doseq [l seq]
+      (.write wr (str l "\n")))))
+
 (defn -main
   "Entry Point"
   [& args]
   (let [insts (->> (remove-whitespace (first args))
                    (filterv not-comment?)
                    (mapv translate-to-binary))]
-    insts))
+    (write-to-file insts (second args))
+    (println "Translated " (first args) " to file " (second args))))
 
-(-main "/home/johnboy14/coursera/nand2tetris/06/add/Add.asm")
+(comment
+  "Assemble Symbol free Assembly programs"
+  (-main
+    "/home/johnboy14/coursera/nand2tetris/06/add/Add.asm"
+    "/home/johnboy14/coursera/nand2tetris/06/add/Add.hack")
+  (-main
+    "/home/johnboy14/coursera/nand2tetris/06/max/MaxL.asm"
+    "/home/johnboy14/coursera/nand2tetris/06/max/MaxL.hack")
+  (-main
+    "/home/johnboy14/coursera/nand2tetris/06/rect/RectL.asm"
+    "/home/johnboy14/coursera/nand2tetris/06/rect/RectL.hack")
+  (-main
+    "/home/johnboy14/coursera/nand2tetris/06/pong/PongL.asm"
+    "/home/johnboy14/coursera/nand2tetris/06/pong/PongL.hack"))
